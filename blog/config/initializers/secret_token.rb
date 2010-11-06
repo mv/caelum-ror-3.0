3002e01f1c3caa004a8b239f9a5701efe91e3305
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-11-06 11:45:48 -0200

    Created: Blog app

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'e5ae7f8e2900325c70ad252af200bf3025b80ba7cbdb7e63ddc716ceb1bee0363653b7458a199091d9ba3034c42b779ca47cd9d3dea553a20da0b2de2d25a19d'
