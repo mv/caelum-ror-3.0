4a76890835fcde5c4a6ca0bc5c61f6f592b637da
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-11-06 11:36:11 -0200

    Added 1

---
+++ temp1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Temp1::Application.config.secret_token = 'e7b3d5a33c547add4802b70d061eb2c1d7f79f869a73dc2931f64560fa1504f4c7e9b77ad3e97f61e758b352a8c94537b64ca3876d5ef23168cd94368bde2dd4'
