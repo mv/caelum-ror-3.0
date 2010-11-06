8cfb7a118e63fefd0d0d286e85501a211b3e5ce3
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-11-06 14:18:28 -0200

    rails generate controller Comments       create  app/controllers/comments_controller.rb       invoke  erb       create    app/views/comments       invoke  test_unit       create    test/functional/comments_controller_test.rb       invoke  helper       create    app/helpers/comments_helper.rb       invoke    test_unit       create      test/unit/helpers/comments_helper_test.rb
    e adicionais....

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   validates :name  , :presence => true
   validates :title , :presence => true
                      # :length   => { :minimun => 5 }
+  
+  has_many :comments
 
 end
 
