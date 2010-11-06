da42a12c40caf3946b92eadb19b3ff19f071c642
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-11-06 20:01:42 -0200

    rails generate model tag name:string post:references       invoke  active_record       create    db/migrate/20101106220104_create_tags.rb       create    app/models/tag.rb       invoke    test_unit       create      test/unit/tag_test.rb       create      test/fixtures/tags.yml
    + rake db:migrate
    
    + tags implementation

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ActiveRecord::Base
                      # :length   => { :minimun => 5 }
   
   has_many :comments, :dependent => :destroy
+  has_many :tags
+
+  accepts_nested_attributes_for :tags, 
+    :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 
 end
 
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101106160934) do
+ActiveRecord::Schema.define(:version => 20101106220104) do
+
+  create_table "c1", :id => false, :force => true do |t|
+    t.integer  "id",         :default => 0, :null => false
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -20,10 +29,26 @@ ActiveRecord::Schema.define(:version => 20101106160934) do
     t.datetime "updated_at"
   end
 
+  create_table "p1", :id => false, :force => true do |t|
+    t.integer  "id",         :default => 0, :null => false
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "posts", :force => true do |t|
     t.string   "name"
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
