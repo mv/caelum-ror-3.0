076619c55ec15dc6884489ba4e0782cf4016e9ec
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-11-06 15:43:47 -0200

    Basic authentication

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def authenticate
+
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == 'admin' && password == 'pass'
+    end # authenticate
+
+  end # def
+
 end
+
--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  before_filter :authenticate, :only => :destroy
+
   # GET /comments
   # GET /comments.xml
   def index
