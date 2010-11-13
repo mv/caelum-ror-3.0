4c44c1ec758fcd007685a6ca9a832d8a54c606e5
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-11-13 09:42:37 -0200

    Restaurante: editing...

--- votaprato/db/migrate/20101113112954_create_restaurantes.rb
+++ votaprato/db/migrate/20101113112954_create_restaurantes.rb
@@ -2,6 +2,9 @@ class CreateRestaurantes < ActiveRecord::Migration
   def self.up
     create_table :restaurantes do |t|
 
+      t.string :name , :limit => 80
+      t.string :endereco , :limit => 80
+
       t.timestamps
     end
   end
