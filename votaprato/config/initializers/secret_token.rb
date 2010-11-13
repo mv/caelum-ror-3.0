edb45091245dd5c139ff457a2676ddd5e3b4e984
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-11-13 09:22:41 -0200

    rails new votaprato -d mysql

---
+++ votaprato/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Votaprato::Application.config.secret_token = 'b066be1e4ed7da43704c6940fca7d3e48ef1a8cb2b28b98f9c90c1418991769901954252fae2f356f89d4000e56efbe59b27c1a6cae61bcd6722841bef26a2ed'
---
+++ votaprato/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Votaprato::Application.config.session_store :cookie_store, :key => '_votaprato_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Votaprato::Application.config.session_store :active_record_store
