e0b22c7a91db34b5339fbf14a5968cc5c69fa8e0
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-11-20 11:09:42 -0200

    Customizations!!!!

--- votaprato/config/environment.rb
+++ votaprato/config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Votaprato::Application.initialize!
 
-config.active_record.observers = :user_observer
+#config.active_record.observers = :user_observer
 
