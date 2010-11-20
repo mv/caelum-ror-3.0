21d824738670aef1bb49d55729bf13d6afc10273
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-11-20 16:21:49 -0200

    Menu Principal

--- votaprato/app/helpers/application_helper.rb
+++ votaprato/app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def menu_principal
+    menu = %w(cliente qualificacao restaurante)
+
+    menu_principal = "<ul>"
+    menu.each do |item|
+
+      menu_principal << "<li>" +
+                        link_to(item, :controller => item.pluralize) +
+                        "</li>"
+
+    end
+    menu_principal << "<ul>"
+
+    raw menu_principal
+
+  end
+
 end
